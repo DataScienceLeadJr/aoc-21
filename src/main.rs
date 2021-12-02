566b406b6f77a49b3b25520c8e0ede705aac1e14
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-12-02 08:09:53 +0100

    changed it to direct day-func calls in match statement.

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,10 @@ use util::{
 };
 
 fn main() {
+    // TODO: figure out how to do this "morpho module" thing...
     let (a, b) = match std::env::args().collect::<Vec<String>>()[1].as_str() {
-        "1st" => (first::a, first::b),
-        "2nd" => panic!("It's coming! IT'S COMING!!"),
+        "1st" => (first::a(&input_file, &store_output), first::b(&input_file, &store_output)),
+        "2nd" => (second::a(&input_file, &store_output), second::b(&input_file, &store_output)),
         _ => panic!("WAIHBEPWYUBEPUB!!! WHAT KINDA DAY IS THAT IN THE MONTH OF DECEMBER!?")
     };
-
-    a(&input_file, &store_output);
-    b(&input_file, &store_output);
-
 }
