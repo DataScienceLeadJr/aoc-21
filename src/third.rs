a0d1ba027a4a1f00d68dda658ce692cfd283b3e4
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-12-03 12:29:00 +0100

    working recursive threaded day 3

--- src/third.rs
+++ src/third.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, panic};
+use std::{collections::{HashMap}, panic, ops::{Range, RangeTo, RangeFrom}};
 
 use itertools::Itertools;
 
@@ -53,7 +53,10 @@ trait Nth {
 
 impl Nth for String {
     fn nth(&self, nth: usize) -> char {
-        self.chars().nth(nth).unwrap()
+        match self.chars().nth(nth) {
+            Some(c) => c,
+            None => panic!("WTF!? self: {:?}\n\tnth:{}", self, nth)
+        }
     }
 }
 
@@ -72,6 +75,56 @@ impl BinarySortMembers for Vec<String> {
     }
 }
 
+trait DifferenceSplitPoint {
+    fn find_difference_point(&self, nth: usize) -> usize;
+}
+impl DifferenceSplitPoint for Vec<String> {
+    fn find_difference_point(&self, nth: usize) -> usize {
+        let mut split_point = 1;
+        while self[split_point - 1].nth(nth) == self[split_point].nth(nth) {
+            split_point += 1;
+        }
+
+        split_point
+    }
+}
+
+trait SplitRange {
+    fn into_split_ranges(self, end: usize) -> (Range<usize>, Range<usize>);
+}
+impl SplitRange for usize {
+    fn into_split_ranges(self, end: usize) -> (Range<usize>, Range<usize>) {
+        ((0..self), (self..end))
+    }
+}
+
+trait MostLeast {
+    fn as_most_least(self) -> Self;
+    fn equal(&self) -> bool;
+    fn switch_on_equal(self) -> Self;
+}
+
+impl MostLeast for (Range<usize>, Range<usize>) {
+    fn equal(&self) -> bool {
+        self.0.len() == self.1.len()
+    }
+    fn as_most_least(self) -> Self {
+        if self.0.len() > self.1.len() {
+            self
+        } else {
+            (self.1, self.0)
+        }
+    }
+
+    fn switch_on_equal(self) -> Self {
+        if self.equal() {
+            (self.1, self.0)
+        } else {
+            self
+        }
+    }
+}
+
 fn check_all_equal(binaries: &[String], bit_of_interest: usize) -> bool {
     let bits: Vec<_> = binaries.iter().map(|s| {
         s.nth(bit_of_interest)
@@ -81,29 +134,57 @@ fn check_all_equal(binaries: &[String], bit_of_interest: usize) -> bool {
     bits.iter().all(|&item| item == first)
 }
 
-fn get_split_point(binaries: &[String], rec: usize) -> usize {
-    let mut split_point = binaries.len() as f32 / 2.0;
-
-    let mut step = split_point * 0.5;
-
-    let mut bit_of_interest = rec;
-
-    let mut left = binaries[split_point as usize - 1].nth(bit_of_interest);
-    let mut right = binaries[split_point as usize].nth(bit_of_interest);
-
-    while left == right{
-
-        // search both ways
-       
-        step *= 0.5;
-        bit_of_interest += 1;
-
-        left = binaries[split_point as usize - 1].nth(bit_of_interest);
-        right = binaries[split_point as usize].nth(bit_of_interest);
+fn search_most(values: Vec<String>, bit_level: usize) -> String {    
+    if values.len() == 1 {
+        return values.first().unwrap().clone();
+    } else if bit_level >= values[0].len() {
+        panic!("most oh no.... \n\tnum_values: {}", values.len());
+    }else if check_all_equal(&values, bit_level) {
+        return search_most(values, bit_level + 1)
     }
 
-    return split_point as usize;
+    let ranges  = values.find_difference_point(bit_level)
+    .into_split_ranges(values.len());
+
+    let most_range = if ranges.equal() {ranges.1} else {ranges.as_most_least().0};
+
+    search_most(values[most_range].to_vec(), bit_level + 1)
 }
+
+fn search_least(values: Vec<String>, bit_level: usize) -> String {
+    if bit_level >= values[0].len() {
+        panic!("least oh no.... \n\tnum_values: {}", values.len());
+    }
+    if values.len() == 1 {
+        return values.first().unwrap().clone();
+    } else if check_all_equal(&values, bit_level) {
+        return search_least(values, bit_level + 1)
+    }
+
+    let ranges  = values.find_difference_point(bit_level)
+    .into_split_ranges(values.len());
+
+    let most_range = if ranges.equal() {ranges.0} else {ranges.as_most_least().1};
+
+    search_least(values[most_range].to_vec(), bit_level + 1)
+}
+
+fn get_most_and_least(values: Vec<String>, bit_level: usize) -> (String, String) {
+    let most_least_ranges = 
+    values
+    .find_difference_point(bit_level)
+    .into_split_ranges(values.len())
+    .as_most_least()
+    .switch_on_equal();
+
+    let most_value = values[most_least_ranges.0].to_vec();
+    let most_handle = std::thread::spawn(move || search_most(most_value, bit_level + 1));
+    let least_value = values[most_least_ranges.1].to_vec();
+    let least_handle = std::thread::spawn(move || search_least(least_value, bit_level + 1));
+
+    (most_handle.join().unwrap(), least_handle.join().unwrap())
+}
+
 
 pub fn b(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(String, &str, TaskPart) -> Result<(), std::io::Error>) {
     println!("Third_B!");
@@ -113,25 +194,19 @@ pub fn b(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(St
     // sort for every bit in one go to make it easier.
     diagnostic_report.rev_bin_sort();
 
-    diagnostic_report.iter().for_each(|n| {println!("{:?}", n)});
-
-    let max_rec = diagnostic_report[0].len();
-
-    let (o2_bin_num, co2_bin_num) = ("110", "001");
+    let (o2_bin_num, co2_bin_num) = get_most_and_least(diagnostic_report.clone(), 0);
     
     let o2_gen_rate = isize::from_str_radix(
-        o2_bin_num,
+        &o2_bin_num,
         2)
         .unwrap() as u32;
 
     let co2_scrub_rate = isize::from_str_radix(
-        co2_bin_num,
+        &co2_bin_num,
         2)
         .unwrap() as u32;
 
     println!("o2: {}, {:b}, co2: {}, {:b}", o2_gen_rate, o2_gen_rate, co2_scrub_rate, co2_scrub_rate);
-
-    assert_eq!(o2_gen_rate * co2_scrub_rate, 230);
 
     store_output((o2_gen_rate * co2_scrub_rate).to_string(), DAY, TaskPart::B).expect("funky task not built right... yet?");
 }
