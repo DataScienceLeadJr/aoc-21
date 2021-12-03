6fe01438695d35c59d59b28ea5ae2894e288ed0f
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-12-03 11:00:23 +0100

    cleaned away old version. introduced good sorting for binary splitting.

--- src/third.rs
+++ src/third.rs
@@ -47,120 +47,85 @@ pub fn a(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(St
     store_output((gamma_rate * epsilon_rate).to_string(), DAY, TaskPart::A).expect("funky task not built right... yet?");
 }
 
-#[derive(Debug, Clone, Copy)]
-enum BinaryCriteria {
-    Most,
-    Least
+trait Nth {
+    fn nth(&self, nth: usize) -> char;
 }
 
-fn get_bit_of_interest(bits: &String, nth: usize) -> char {
-    bits.chars().nth(nth).unwrap()
+impl Nth for String {
+    fn nth(&self, nth: usize) -> char {
+        self.chars().nth(nth).unwrap()
+    }
+}
+
+trait BinarySortMembers {
+    fn rev_bin_sort(&mut self);
+}
+
+impl BinarySortMembers for Vec<String> {
+    fn rev_bin_sort(&mut self) {
+        (0..self.len()).rev().for_each(
+          |i| {
+            self.sort_by(|a, b| 
+                a.chars().nth(i).partial_cmp(&b.chars().nth(i)).unwrap())
+          }
+        );
+    }
 }
 
 fn check_all_equal(binaries: &[String], bit_of_interest: usize) -> bool {
     let bits: Vec<_> = binaries.iter().map(|s| {
-        get_bit_of_interest(s, bit_of_interest)
+        s.nth(bit_of_interest)
     }).collect();
     
     let first = bits[0];
     bits.iter().all(|&item| item == first)
 }
 
-fn get_split_point(binaries: &[String], crit: BinaryCriteria, rec: usize) -> usize {
+fn get_split_point(binaries: &[String], rec: usize) -> usize {
     let mut split_point = binaries.len() as f32 / 2.0;
 
     let mut step = split_point * 0.5;
 
     let mut bit_of_interest = rec;
 
-    let mut left = get_bit_of_interest(&binaries[split_point as usize - 1], bit_of_interest);
-    let mut right = get_bit_of_interest(&binaries[split_point as usize], bit_of_interest);
+    let mut left = binaries[split_point as usize - 1].nth(bit_of_interest);
+    let mut right = binaries[split_point as usize].nth(bit_of_interest);
 
     while left == right{
-        match crit {
-            BinaryCriteria::Most => {
-                if left == '0' {
-                    split_point += step;
-                } else {
-                    split_point -= step;
-                }
-            }
-            BinaryCriteria::Least => {
-                if left == '0' {
-                    split_point -= step;
-                } else {
-                    split_point += step;
-                }
-            }
-        }
+
+        // search both ways
+       
         step *= 0.5;
         bit_of_interest += 1;
 
-        if split_point < 1.0 {
-            println!("split point not big enough!! {}", split_point);
-            println!("crit: {:?} boi: {}", crit, bit_of_interest);
-            println!("bins: {:?}", binaries);
-        }
-
-        left = get_bit_of_interest(&binaries[split_point as usize - 1], bit_of_interest);
-        right = get_bit_of_interest(&binaries[split_point as usize], bit_of_interest);
+        left = binaries[split_point as usize - 1].nth(bit_of_interest);
+        right = binaries[split_point as usize].nth(bit_of_interest);
     }
 
     return split_point as usize;
-}
-
-fn bin_search(binaries: &mut [String], crit: BinaryCriteria, max_rec: usize, rec: usize) -> &[String] {
-    if binaries.len() <= 1 {
-        return binaries;
-    } else if rec > max_rec {
-        println!("has criteria: {:?}", crit);
-        panic!("reach rec {}! Still has binaries: {:?}", &rec, binaries);
-    } else if check_all_equal(binaries, rec) {
-        binaries.sort_by(|a, b| a.chars().nth(rec).partial_cmp(&b.chars().nth(rec)).unwrap());
-        return bin_search(binaries, crit, max_rec, rec + 1)
-    }
-
-    let split_point = get_split_point(&binaries, crit, rec);
-
-    match crit {
-        BinaryCriteria::Most => {
-            let sub_slize = 
-            if binaries[split_point..].len() == binaries[..split_point].len() {
-                  &mut binaries[..split_point]
-            } else {
-                &mut binaries[split_point..]
-            };
-            sub_slize.sort_by(|a, b| a.chars().nth(rec).partial_cmp(&b.chars().nth(rec)).unwrap());
-            bin_search(sub_slize, crit, max_rec, rec + 1)
-        }
-        BinaryCriteria::Least => {
-            let sub_slize = 
-            if binaries[split_point..].len() == binaries[..split_point].len() {
-                  &mut binaries[split_point..]
-            } else {
-                &mut binaries[..split_point]
-            };
-            sub_slize.sort_by(|a, b| a.chars().nth(rec).partial_cmp(&b.chars().nth(rec)).unwrap());
-            bin_search(sub_slize, crit, max_rec, rec + 1)
-        }
-    }
 }
 
 pub fn b(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(String, &str, TaskPart) -> Result<(), std::io::Error>) {
     println!("Third_B!");
 
     let mut diagnostic_report = process_input_day_1(load_input(DAY, TaskPart::B));
-    diagnostic_report.sort();
+
+    // sort for every bit in one go to make it easier.
+    diagnostic_report.rev_bin_sort();
+
+    diagnostic_report.iter().for_each(|n| {println!("{:?}", n)});
 
     let max_rec = diagnostic_report[0].len();
+
+    let (o2_bin_num, co2_bin_num) = ("110", "001");
     
     let o2_gen_rate = isize::from_str_radix(
-        &bin_search(&mut diagnostic_report.clone(), BinaryCriteria::Most, max_rec, 0)[0],
+        o2_bin_num,
         2)
         .unwrap() as u32;
 
     let co2_scrub_rate = isize::from_str_radix(
-        &bin_search(&mut diagnostic_report, BinaryCriteria::Least, max_rec, 0)[0],
+        co2_bin_num,
         2)
         .unwrap() as u32;
 
