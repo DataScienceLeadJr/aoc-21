565bf34d32f1a5f62f5708473b489c438352ee74
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-12-03 09:12:26 +0100

    done 3rd part a

--- src/third.rs
+++ src/third.rs
@@ -1,73 +1,81 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
 use crate::util::TaskPart;
 
-pub const DAY: &str = "2nd";
+pub const DAY: &str = "3rd";
 
-enum Direction{
-    Forward,
-    Up,
-    Down,
-}
 
-fn process_input_day_1(input: String) -> Vec<(Direction, i32)> {
+fn process_input_day_1(input: String) -> Vec<String> {
     input
     .lines()
-    .map(|command| {
-        let parts = command.split(" ").collect::<Vec<&str>>();
-        let direction = match parts[0] {
-            "forward" => Direction::Forward,
-            "down" => Direction::Down,
-            "up" => Direction::Up,
-            _ => panic!("WTF DIRECTION IS THAT!?"),
-        };
-        let amount = parts[1].parse::<i32>().unwrap();
-        (direction, amount)
-    })
-    .collect::<Vec<(Direction, i32)>>()
+    .map(|string| string.to_string())
+    .collect()
 }
 
 pub fn a(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(String, &str, TaskPart) -> Result<(), std::io::Error>) {
-    println!("Second_A!");
+    println!("Third_A!");
 
-    let commands = process_input_day_1(load_input(DAY, TaskPart::A));
-    let mut hor_pos = 0;
-    let mut depth = 0;
-    let _result: () = commands.iter().map(|(dir, amount)| {
-        match dir {
-            Direction::Forward => {hor_pos += amount;}
-            Direction::Down => {depth += amount;}
-            Direction::Up => {depth -= amount;}
-        }
-    }).collect();
+    let diagnostic_report = process_input_day_1(load_input(DAY, TaskPart::A));
 
-    println!("hp: {}, d: {}", hor_pos, depth);
+    let mut bit_counts: HashMap<usize, (usize, usize)> = HashMap::with_capacity(diagnostic_report[0].len());
 
-    store_output((hor_pos * depth).to_string(), DAY, TaskPart::A).expect("funky task not built right... yet?");
+    for bin_num in diagnostic_report.into_iter() {
+        let _: () = bin_num.chars().enumerate().map(|(i, c)| {
+            if c == '1' {
+                bit_counts.entry(i).or_insert((0, 0)).1 += 1;
+            } else if c == '0' {
+                bit_counts.entry(i).or_insert((0, 0)).0 += 1;
+            } else {
+                panic!("what the fuck is This: {}", c);
+            }
+        }).collect();
+    }
+
+    let mut bin_num: String = String::new();
+
+    for (_cardinality, counts) in bit_counts.iter().sorted() {
+        bin_num += if counts.0 > counts.1 {"0"} else {"1"};
+    }
+    
+    let gamma_rate = isize::from_str_radix(&bin_num, 2).unwrap() as u32;
+    let epsilon_rate = gamma_rate ^ (2_u32.pow(bin_num.len() as u32) - 1) as u32;
+
+    println!("hp: {}, {:b}, d: {}, {:b}", gamma_rate, gamma_rate, epsilon_rate, epsilon_rate);
+
+    store_output((gamma_rate * epsilon_rate).to_string(), DAY, TaskPart::A).expect("funky task not built right... yet?");
 }
 
 pub fn b(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(String, &str, TaskPart) -> Result<(), std::io::Error>) {
-    println!("Second_B!");
+    println!("Third_B!");
 
-    let commands = process_input_day_1(load_input(DAY, TaskPart::B));
+    let diagnostic_report = process_input_day_1(load_input(DAY, TaskPart::B));
 
-    let mut hor_pos = 0;
-    let mut depth = 0;
-    let mut aim = 0;
-    let _result: () = commands.iter().map(|(dir, amount)| {
-        match dir {
-            Direction::Forward => {
-                hor_pos += amount;
-                depth += aim * amount;
+    let mut bit_counts: HashMap<usize, (usize, usize)> = HashMap::with_capacity(diagnostic_report[0].len());
+
+    for bin_num in diagnostic_report.into_iter() {
+        let _: () = bin_num.chars().enumerate().map(|(i, c)| {
+            if c == '1' {
+                bit_counts.entry(i).or_insert((0, 0)).1 += 1;
+            } else if c == '0' {
+                bit_counts.entry(i).or_insert((0, 0)).0 += 1;
+            } else {
+                panic!("what the fuck is This: {}", c);
             }
-            Direction::Down => {
-                aim += *amount;
-            }
-            Direction::Up => {
-                aim -= amount;
-            }
-        }
-    }).collect();
+        }).collect();
+    }
 
-    println!("hp: {}, d: {}", hor_pos, depth);
+    let mut bin_num: String = String::new();
 
-    store_output((hor_pos * depth).to_string(), DAY, TaskPart::B).expect("funky task not built right... yet?");
+    for (_cardinality, counts) in bit_counts.iter().sorted() {
+        bin_num += if counts.0 > counts.1 {"0"} else {"1"};
+    }
+    
+    let gamma_rate = isize::from_str_radix(&bin_num, 2).unwrap() as u32;
+    let epsilon_rate = gamma_rate ^ (2_u32.pow(bin_num.len() as u32) - 1) as u32;
+
+    println!("hp: {}, {:b}, d: {}, {:b}", gamma_rate, gamma_rate, epsilon_rate, epsilon_rate);
+
+    store_output((gamma_rate * epsilon_rate).to_string(), DAY, TaskPart::B).expect("funky task not built right... yet?");
 }
