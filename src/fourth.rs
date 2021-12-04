5797fadd52112b4f51742321c65bc2c60aa88c1d
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-12-04 10:47:08 +0100

    made the input processing.

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,23 +1,36 @@
 use std::{collections::{HashMap}, panic, ops::{Range, RangeTo, RangeFrom}};
 
-use itertools::Itertools;
-
 use crate::util::TaskPart;
 
 pub const DAY: &str = "4th";
 
+type BingoBoards = Vec<[[(i32, bool); 5]; 5]>;
 
-fn process_input_day_1(input: String) -> Vec<String> {
-    input
-    .lines()
-    .map(|string| string.to_string())
-    .collect()
+
+fn process_input_day_1(input: String) -> (Vec<i32>, BingoBoards) {
+    let mut lines: Vec<_> = input.lines().collect();
+    let draw_numbers = lines.first().unwrap().split(",").map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    lines = lines[1..].to_vec();
+
+    let mut boards: BingoBoards = Vec::new();
+
+    let mut current_board = [[(-1, false); 5]; 5];
+
+    for board_index in (0..lines.len()).step_by(6) {
+        for board_line in 1..6 {
+            lines[board_index + board_line].split_whitespace().enumerate().for_each(|(i, num)| {current_board[board_line-1][i] = (num.parse().unwrap(), false);});
+        }
+        boards.push(current_board);
+        current_board = [[(-1, false); 5]; 5];
+    }
+
+    (draw_numbers, boards)
 }
 
 pub fn a(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(String, &str, TaskPart) -> Result<(), std::io::Error>) {
     println!("Fourth_A!");
 
-    let diagnostic_report = process_input_day_1(load_input(DAY, TaskPart::A));
+    let (draw_numbers, boards) = process_input_day_1(load_input(DAY, TaskPart::A));
 
     // Here we Go!
 
