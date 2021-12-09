ad212bb9b90e74628200f0f96d8f512a0a9d3fd5
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-12-09 16:07:09 +0100

    done day 9 part b.

--- src/ninth.rs
+++ src/ninth.rs
@@ -1,3 +1,5 @@
+use std::iter::Product;
+
 use itertools::Itertools;
 
 use crate::util::TaskPart;
@@ -26,8 +28,16 @@ impl Heightmap {
         });
     }
 
+    fn filter_locations(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        self.filter.iter().map(|neighbour| ((x as i32 + neighbour.0) as usize, (y as i32 + neighbour.1) as usize)).collect()
+    }
+
+    fn filter_values(&self, x: usize, y: usize) -> Vec<i32> {
+        self.filter.iter().map(|neighbour| self.map[(x as i32 + neighbour.0) as usize][(y as i32 + neighbour.1) as usize]).collect()
+    }
+
     fn is_lowpoint(&self, x: usize, y: usize) -> bool {
-        self.map[x][y] < self.filter.iter().map(|neighbour| self.map[(x as i32 + neighbour.0) as usize][(y as i32 + neighbour.1) as usize]).min().unwrap()
+        self.map[x][y] < *self.filter_values(x, y).iter().min().unwrap()
     }
 
     fn run_filter(&self) -> Vec<((usize, usize), i32)> {
@@ -41,6 +51,46 @@ impl Heightmap {
         }
 
         filter_result
+    }
+
+    fn expand_frontline(&self, frontline: &mut Vec<(usize, usize)>, x: usize, y: usize, already_searched: &Vec<(usize, usize)>) {
+        frontline.append(
+            &mut self.filter_values(x,y)
+            .into_iter()
+            .zip(
+                self.filter_locations(x, y))
+            .filter(
+                |(value, loc)| *value < 9 && !(already_searched.contains(loc) || frontline.contains(loc)))
+            .map(
+                |(_, loc)| loc)
+            .collect::<Vec<(usize, usize)>>())
+    }
+
+    fn bfs_from_lowpoint(&self, lowpoint: (usize, usize)) -> Vec<i32>{
+        let mut basin = vec![self.map[lowpoint.0][lowpoint.1]];
+        let mut already_searched = vec![lowpoint];
+        let mut frontline: Vec<(usize, usize)> = Vec::new();
+        self.expand_frontline(&mut frontline, lowpoint.0, lowpoint.1, &already_searched);
+
+        // println!("started expanding basin! \n\tstarting loc-val: {:?}_{}\n\tfrontline len: {}", lowpoint, basin.first().unwrap(), frontline.len());
+
+        while !frontline.is_empty() {
+            let loc = frontline.remove(0);
+            // println!("\tsearching and pushing: {:?}, {}", loc, self.map[loc.0][loc.1]);
+            basin.push(self.map[loc.0][loc.1]);
+            already_searched.push(loc);
+            // let fl_len = frontline.len();
+            self.expand_frontline(&mut frontline, loc.0, loc.1, &already_searched);
+            // println!("expanded frontline by: {}", frontline.len() - fl_len);
+        }
+        // println!("done with basin! len: {}", basin.len());
+        basin
+    }
+
+    fn grow_into_basins(&self, lowpoints: Vec<(usize, usize)>) -> Vec<i32> {
+        lowpoints.into_iter().map(|lowpoint| {
+            self.bfs_from_lowpoint(lowpoint).len() as i32
+        }).collect()
     }
 }
 
@@ -57,10 +107,8 @@ pub fn a(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(St
         ],
     };
 
-    height_map.pad(10);
+    height_map.pad(9);
     let low_points = height_map.run_filter();
-
-    println!("low points: {:?}", low_points);
 
     let the_answer: i32 = low_points.iter().map(|(_, val)| val + 1).sum();
 
@@ -82,12 +130,18 @@ pub fn b(load_input: &dyn Fn(&str, TaskPart) -> String, store_output: &dyn Fn(St
         ],
     };
 
-    height_map.pad(10);
+    height_map.pad(9);
     let low_points = height_map.run_filter();
 
-    println!("low points: {:?}", low_points);
 
-    let the_answer: i32 = low_points.iter().map(|(_, val)| val + 1).sum();
+    let mut basin_sizes = height_map.grow_into_basins(
+        low_points.into_iter().map(|(loc, _)| loc).collect()
+    );
+    basin_sizes.sort();
+    println!("Basin sizes: {:?}", basin_sizes);
+
+    let the_answer: i32 = basin_sizes[basin_sizes.len() - 3..].to_owned().into_iter().product();
+
 
     println!("the answer is {}", the_answer);
 
